87847c91b3b562f4e06913762d2fb618d8fb6e5d
henryno111 <h.agukwe@yahoo.com>
2023-06-13 17:49:01 -0700

    task 3

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
+"""Defines a file-appending function."""
+
 
 def append_write(filename="", text=""):
+    """Appends a string to the end of a UTF8 text file.
 
-    with open(filename, encoding="utf-8") as f:
-        if nb_lines <= 0:
-            print(f.read(), end="")
-            return
-
-        lines = 0
-        for line in f:
-            lines += 1
-        f.seek(0)
-        if nb_lines >= lines:
-            print(f.read(), end="")
-
-        else:
-            n = 0
-            while n < nb_lines:
-                print(f.readline(), end="")
-                n += 1
+    Args:
+        filename (str): The name of the file to append to.
+        text (str): The string to append to the file.
+    Returns:
+        The number of characters appended.
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
