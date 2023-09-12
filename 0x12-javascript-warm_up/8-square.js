af70d006f026fb107ea3e710e6a616f9f0c5a0de
Henryno111 <h.agukwe@yahoo.com>
2023-09-12 10:17:57 -0700

    final update

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -10,7 +10,7 @@ function printSquare (size) {
     for (let i = 0; i < size; i++) {
       let row = '';
       for (let j = 0; j < size; j++) {
-        row += 'x ';
+        row += 'X';
       }
       square += row + '\n';
     }
