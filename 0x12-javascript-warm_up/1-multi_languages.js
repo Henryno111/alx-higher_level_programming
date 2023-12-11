af8a48f79c2597eb397fbdc1a1a13e6b0c0e8e41
Agukwe henry <h.agukwe@yahoo.com>
2023-12-11 14:14:35 -0800

    task 1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const sprintOne = 'C is fun';
+const sprintTwo = 'Python is cool';
+let sprintThree = 'JavaScript is amazing';
+
+console.log(sprintOne);
+console.log(sprintTwo);
+console.log(sprintThree);
